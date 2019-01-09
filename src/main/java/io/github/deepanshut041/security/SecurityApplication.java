7c40d100ddd2bd3014e082b6110f3c5163348c98
deepanshut041 <deepanshut041@gmail.com>
2019-01-09 22:42:05 +0530

    Added oauth2 now working on adding jwt to it

--- src/main/java/io/github/deepanshut041/security/SecurityApplication.java
+++ src/main/java/io/github/deepanshut041/security/SecurityApplication.java
@@ -1,13 +1,25 @@
 package io.github.deepanshut041.security;
 
+import io.github.deepanshut041.security.repositories.UserRepository;
+import io.github.deepanshut041.security.service.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Autowired
+	public void authenticationManager(AuthenticationManagerBuilder builder, UserRepository repo) throws Exception {
+		builder.userDetailsService(username -> new CustomUserDetails(repo.findByUsername(username)));
 	}
 
 }
---
+++ src/main/java/io/github/deepanshut041/security/config/ResourceServerConfig.java
@@ -0,0 +1,20 @@
+package io.github.deepanshut041.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+
+@EnableResourceServer
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/private/**").authenticated();
+
+    }
+}
